66db6adae9061137cec8797a7e99e8a82a849af8
Bryan <bryanmotta7@mail.com>
2019-08-21 20:16:04 -0300

    .

---
+++ src/main/java/br/com/bitbank/configuracao/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package br.com.bitbank.configuracao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import br.com.bitbank.dao.UsuarioDAO;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	
+	@Autowired
+    private UsuarioDAO usuarioDao;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) 
+            throws Exception {
+
+        auth.userDetailsService(usuarioDao)
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }
+    
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+
+		
+	    http.authorizeRequests()
+	    .antMatchers("/resources/**").permitAll()
+	    .antMatchers("/cliente/lista").hasRole("ADMIN")
+	    .antMatchers("/transacoes/**").authenticated()
+	    .antMatchers("/novo-cliente/cadastrar").permitAll()
+	    .antMatchers("/").permitAll()
+	    .anyRequest().authenticated()
+	    .and().formLogin().loginPage("/login")
+        .defaultSuccessUrl("/").permitAll()
+    .and().logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .permitAll().logoutSuccessUrl("/login"); 
+	}
+}
---
+++ src/main/java/br/com/bitbank/modelo/DadosDeposito.java
@@ -0,0 +1,19 @@
+package br.com.bitbank.modelo;
+
+public class DadosDeposito {
+
+	private Double value;
+
+	public DadosDeposito(Double value) {
+		this.value = value;
+	}
+	public DadosDeposito() {
+		
+	}
+	
+	public Double getValue() {
+        return value;
+    }
+	
+	
+}
