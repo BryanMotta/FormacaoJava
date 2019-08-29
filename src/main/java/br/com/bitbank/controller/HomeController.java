0ffdbcae44adf9366c0b51cbbe010be615626864
Bryan Motta <bryanmotta7@gmail.com>
2019-08-29 16:36:48 -0300

    heroku plugin

--- src/main/java/br/com/bitbank/controller/HomeController.java
+++ src/main/java/br/com/bitbank/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController{
 
-    @RequestMapping(value = "/",name = "home" )
+    @RequestMapping(value = "/" )
     public String index(){
         System.out.println("Entrando na home do bitbank");
         return "home";
