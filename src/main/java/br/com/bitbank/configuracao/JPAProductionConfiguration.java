b6b745aeedcf6f0183b34696bc357cf01d3128a5
Bryan Motta <bryanmotta7@gmail.com>
2019-09-05 03:50:48 -0300

    .

--- src/main/java/br/com/bitbank/configuracao/JPAProductionConfiguration.java
+++ src/main/java/br/com/bitbank/configuracao/JPAProductionConfiguration.java
@@ -23,7 +23,7 @@ public class JPAProductionConfiguration {
     @Bean
     public Properties additionalProperties() {
         Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.Postgre9Dialect");
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.Postgre95Dialect");
         props.setProperty("hibernate.show_sql", "true");
         props.setProperty("hibernate.hbm2ddl.auto", "create");
         
