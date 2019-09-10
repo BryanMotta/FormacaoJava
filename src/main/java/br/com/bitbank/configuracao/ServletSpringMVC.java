c2e80699820447977c3f5290941d48f8d1e04752
Bryan Motta <bryanmotta7@gmail.com>
2019-09-10 07:34:02 -0300

    .

--- src/main/java/br/com/bitbank/configuracao/ServletSpringMVC.java
+++ src/main/java/br/com/bitbank/configuracao/ServletSpringMVC.java
@@ -2,8 +2,11 @@ package br.com.bitbank.configuracao;
 
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -13,7 +16,7 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[]{SecurityConfiguration.class, 
-				AppWebConfiguration.class, 
+				AppWebConfiguration.class, JPAconfiguracao.class,
 				JPAProductionConfiguration.class};
 	}
 
@@ -39,12 +42,12 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		   registration.setMultipartConfig(new MultipartConfigElement(""));
     }
 	
-//	@Override
-//	public void onStartup(ServletContext servletContext) throws ServletException {
-//	    super.onStartup(servletContext);
-//	    servletContext.addListener(RequestContextListener.class);
-//	    servletContext.setInitParameter("spring.profiles.active", "dev");
-//	}
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+	    super.onStartup(servletContext);
+	    servletContext.addListener(RequestContextListener.class);
+	    servletContext.setInitParameter("spring.profiles.active", "dev");
+	}
 	
 
 
