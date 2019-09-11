9a89ab0d52432510054ab0cfcaff10300fd324b8
Bryan Motta <bryanmotta7@gmail.com>
2019-09-10 23:26:06 -0300

    .

--- src/main/java/br/com/bitbank/configuracao/JPAProductionConfiguration.java
+++ src/main/java/br/com/bitbank/configuracao/JPAProductionConfiguration.java
@@ -20,52 +20,52 @@ public class JPAProductionConfiguration {
 	@Autowired
 	private Environment environment;
 
-//    @Bean
-//    public Properties additionalProperties() {
-//        Properties props = new Properties();
-//        props.setProperty("hibernate.dialect", "org.hibernate.dialect.ProgressDialect");
-//        props.setProperty("hibernate.show_sql", "true");
-//        props.setProperty("hibernate.hbm2ddl.auto", "create"); 
-//        
-//        
-//        return props;
-//    }
-
-	@Bean
-	private Properties additionalProperties() {
-		Properties props = new Properties();
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl.auto", "create");
-
-		return props;
-	}
+    @Bean
+    public Properties additionalProperties() {
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.ProgressDialect");
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "create"); 
+        
+        
+        return props;
+    }
 
 //	@Bean
-//	public DataSource dataSource() throws URISyntaxException {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName("org.postgresql.Driver");
+//	private Properties additionalProperties() {
+//		Properties props = new Properties();
+//		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+//		props.setProperty("hibernate.show_sql", "true");
+//		props.setProperty("hibernate.hbm2ddl.auto", "create");
 //
-//		URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-//		dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath());
-//		dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-//		dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-//
-//		return dataSource;
+//		return props;
 //	}
-	
+
 	@Bean
 	public DataSource dataSource() throws URISyntaxException {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		
-		URI dbUrl = new URI(environment.getProperty("JAWSDB_URL"));
-		dataSource.setUrl("jdbc:mysql2://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath());
+		dataSource.setDriverClassName("org.postgresql.Driver");
+
+		URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
+		dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath());
 		dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
 		dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-		
+
 		return dataSource;
 	}
+	
+//	@Bean
+//	public DataSource dataSource() throws URISyntaxException {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		
+//		URI dbUrl = new URI(environment.getProperty("JAWSDB_URL"));
+//		dataSource.setUrl("jdbc:mysql2://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath());
+//		dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+//		dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
+//		
+//		return dataSource;
+//	}
 
 
 
